51ad527c707adeae04955fb9b0713508c5b8f7b6
rahulganta <gantarahul@gmail.com>
2021-01-13 16:42:34 -0500

    RG-00 Course page

--- myschool-admin-ui/src/views/Course.vue
+++ myschool-admin-ui/src/views/Course.vue
@@ -21,7 +21,15 @@
       <div class="tab-pane fade show active" id="pills-messages" role="tabpanel" aria-labelledby="pills-messages-tab">
         <div class="card mi-card h-100">
           <div class="card-body">
-            <h5 class="card-title">Course Messages</h5>
+            <div class="row">
+              <div class="col-lg-6 col-sm-12 col-xs-12">
+                <h5 class="card-title">Course Messages</h5>
+              </div>
+              <div class="col-lg-6 col-sm-12 col-xs-12 text-right">
+                <button id="addcoursemessage" class="btn mi-linkbtn" @click="showModal('courseMessageModal', 'add', initialCourseMessage)" :aria-expanded="showAddCourseMessageModal ? 'true':'false'">
+                  <i class="fas fa-plus"/> ADD CLASS ANNOUNCEMENT</button>
+              </div>
+            </div>
             <ul class="list-unstyled">
               <li class="media mb-3" v-for="(courseMessage, index) in courseMessages">
                 <!--<i class="far fa-circle fa-2x mr-2"/>-->
@@ -30,6 +38,16 @@
                   <strong class="mt-0 mb-1">{{courseMessage.subject}}</strong>
                   <div>{{courseMessage.message}}</div>
                   <p class="text-muted">Posted on: {{ courseMessage.createdTimeStamp | formatDateTime }}</p>
+                </div>
+                <div class="mi-menu dropdown">
+                  <a class="btn mi-linkbtn" href="#" role="button" id="dropdownMenuButton" name="dropdown" data-toggle="dropdown" aria-haspopup="true"
+                     aria-expanded="false"> <i class="fas fa-ellipsis-v"></i>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right mi-dropdown-menu " aria-labelledby="dropdownMenuButton">
+                    <button class="dropdown-item" name="edit" @click="showModal('courseMessageModal', 'update', courseMessage)" :aria-expanded="showAddCourseMessageModal ? 'true':'false'"><i class="fas fa-pen pr-1"/> Edit</button>
+                    <hr>
+                    <button class="dropdown-item" name="delete"><i class="fas fa-ban pr-1"/> Delete</button>
+                  </div>
                 </div>
               </li>
             </ul>
@@ -60,37 +78,61 @@
         <div class="card mi-card h-100">
           <div class="card-body">
             <h5 class="card-title">Students</h5>
-            <ul class="list-unstyled">
-              <li class="media mb-3" v-for="(courseMessage, index) in courseMessages">
-                <!--<i class="far fa-circle fa-2x mr-2"/>-->
-                <div class="numberCircle mr-3" style="border-color: #37966f" ><span style="color: #37966f">{{ index+1 }}</span></div>
-                <div class="media-body">
-                  <strong class="mt-0 mb-1">{{courseMessage.subject}}</strong>
-                  <div>{{courseMessage.message}}</div>
-                  <p class="text-muted">Posted on: {{ courseMessage.createdTimeStamp | formatDateTime }}</p>
-                </div>
-              </li>
-            </ul>
+            <MiTable :columns-headers="studentTableColumnsHeaders" :data-list="studentList" :row-actions="studentTableRowActions" :table-actions="studentTableActions" :show-row-check-box="true"
+                     @rowAaction="tableRowAction" @tableAction="tableAction"></MiTable>
           </div>
         </div>
       </div>
 
     </div>
+
+    <AddCourseMessage v-if="showAddCourseMessageModal" @close="close" :message="courseMessage" :action="action" @addMessage="addCourseMessage"></AddCourseMessage>
+    <!--Toast-->
+    <Toasts></Toasts>
   </div>
 </template>
 
 <script>
-const API_URL = "/api/myschool/";
+import AddCourseMessage from "@/components/modals/AddCourseMessage";
+import MiTable from "@/components/MiTable";
+
 export default {
   name: "Course",
   components: {
-
+    AddCourseMessage,
+    MiTable
   },
   data() {
     return {
       errorMsg: '',
+      action: 'add',
+      showAddCourseMessageModal: false,
+      school: this.$store.state.user.school,
       course: {},
       courseMessages:[],
+      initialCourseMessage: {
+        subject: '',
+        message: '',
+        priority: 'important',
+        courseId: 0
+      },
+      courseMessage: {},
+      studentList: [],
+      studentTableColumnsHeaders: [
+        { title: "USERNAME", sortKey: "username", sortOrder: 1, action: "viewschool", selectedFilters: []},
+        { title: "FIRST NAME", sortKey: "firstName", sortOrder: 1, selectedFilters: [], hideOnScr: 'sm'},
+        { title: "LAST NAME", sortKey: "lastName", sortOrder: 1, selectedFilters: [], },
+        { title: "EMAIL", sortKey: "email", sortOrder: 1, selectedFilters: [],hideOnScr: 'sm'},
+      ],
+      studentTableRowActions: [
+        { title: "View Student", name: "viewstudent", icon: "fa-info-circle"},
+        { title: "Edit Student", name: "editstudent", icon: "fa-pen"},
+        { title: "Delete Student", name: "deletestudent", icon: "fa-trash"},
+      ],
+      studentTableActions: [
+        { title: "ADD STUDENT", name: "addstudent", icon: "fa-plus"},
+        { title: "DELETE STUDENT", name: "deletestudent", icon: "fa-trash"},
+      ],
     }
   },
   created() {
@@ -99,12 +141,13 @@ export default {
       this.getCourse();
     }
     this.getCourseMessages();
+    this.getCourseStudents();
   },
   methods: {
     getCourse() {
       let vm = this;
       let courseId = this.$route.params.id;
-      this.axios.get(API_URL+ "course/"+courseId, this.restCallHeaders()).then(
+      this.axios.get(this.$constants().BASE_URL + "course/"+courseId, this.restCallHeaders()).then(
           response => {
             vm.course = response.data;
             vm.$store.commit('saveCourse', {course: vm.course});
@@ -116,7 +159,7 @@ export default {
     getCourseMessages() {
       let vm = this;
       let courseId = this.$route.params.id;
-      this.axios.get(API_URL+ "messages/allcoursemessages/"+courseId, this.restCallHeaders()).then(
+      this.axios.get(this.$constants().BASE_URL + "messages/allcoursemessages/"+courseId, this.restCallHeaders()).then(
           response => {
             vm.courseMessages = response.data;
           },
@@ -124,12 +167,56 @@ export default {
             this.errorMsg = error.response.message;
           });
     },
+    addCourseMessage() {
+      this.getCourseMessages();
+
+    },
+    getCourseStudents() {
+      let vm = this;
+      let school = this.$store.state.school;
+      if(school) {
+        vm.school = school;
+      }
+      this.axios.get(this.$constants().BASE_URL + "students/"+vm.school.id, this.restCallHeaders()).then(
+          response => {
+            vm.studentList = response.data;
+            vm.errorMsg = '';
+          },
+          error => {
+            vm.errorMsg = error.response.error +": " + error.message;
+          });
+    },
+    tableRowAction(actionName, rowData, index) {
+      console.log("Row action is: "+actionName+ " Row data is: " +rowData.username+ " Index is: "+index);
+    },
+    tableAction(actionName, selectedTableData) {
+      if (Array.isArray(selectedTableData) && (selectedTableData.length == 0)) {
+        console.log("Empty selected data");
+      } else {
+        console.log("Table Action is: " +actionName+ " Table data is: "+selectedTableData);
+      }
+    },
+    showModal(modal, action, data) {
+      this.action = action;
+      this.courseMessage = JSON.parse(JSON.stringify(data));
+      this.courseMessage.courseId = this.course.id;
+      if(modal == 'courseMessageModal') {
+        this.showAddCourseMessageModal = true;
+      }
+
+    },
+    close() {
+      this.showAddCourseMessageModal = false;
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
   a.mi-linkbtn.active {
 
+  }
+  .media-body {
+    /*border-bottom: 1px solid #9aa2a87d !important;*/
   }
 </style>
